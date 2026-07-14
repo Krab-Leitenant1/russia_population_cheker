e49cced7f76204b3bd0ee13c886f639bd495ff99
Krab-Leitenant <timsottim@mail.ru>
2026-07-14 13:48:17 +0300

    Backend part of the project

---
+++ main.py
@@ -0,0 +1,12 @@
+from parser import Parser
+
+import schedule
+def main():
+    p = Parser('db', 'https://www.worldometers.info/world-population/russia-population/')
+    p.setup()
+    schedule.every().day.at("12:00").do(p.parse)
+    while True:
+        schedule.run_pending()
+if __name__ == "__main__":
+    main()
+
---
+++ parser.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import sqlite3
+import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Parser():
+    def __init__(self, base, url):
+        self.base = base
+        self.url = url
+
+    def setup(self):
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.options.add_argument("--window-size=1,1")
+        self.driver = webdriver.Chrome(options=self.options)
+        with sqlite3.connect(f"{self.base}.sqlite3") as db:
+            cursor = db.cursor()
+            cursor.executescript(
+                '''CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY , count INTEGER, date DATE)''')
+    def parse(self):
+        self.driver.get(self.url)
+        el = self.driver.find_element(By.CLASS_NAME, 'rts-counter')
+
+        count = int(el.text.replace(',',''))
+        date = str(datetime.date.today())
+        WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, 'rts-counter')))
+        self.driver.quit()
+        with sqlite3.connect(f"{self.base}.sqlite3") as db:
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO data(count, date) VALUES(?, ?)", (count, date))
